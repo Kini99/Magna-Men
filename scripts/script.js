fcf6bd5579a8181a4bf0657aea7881dc690d5a57
Kini99 <kinjalmomaya99@gmail.com>
2023-02-20 22:48:42 +0530

    backend server deployed

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// --- do not touch  ↓↓↓↓↓↓↓↓↓↓↓↓ ----------
+const baseServerURL = `http://localhost:${
+  import.meta.env.REACT_APP_JSON_SERVER_PORT
+}`;
+// --- do not touch  ↑↑↑↑↑↑↑↑↑↑↑↑ ----------
