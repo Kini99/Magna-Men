5f4be7e00e0609ded7d7155ef1781c37798296be
Kinjal Momaya <118278010+Kini99@users.noreply.github.com>
2023-02-23 23:16:18 +0530

    Delete script.js

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-// --- do not touch  ↓↓↓↓↓↓↓↓↓↓↓↓ ----------
-const baseServerURL = `https://63f45eca3f99f5855dae29dc.mockapi.io`;
-// --- do not touch  ↑↑↑↑↑↑↑↑↑↑↑↑ ----------
-
-// /products
-// /users
