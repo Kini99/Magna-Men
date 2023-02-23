63f8855bd28c56caf95c6853526f91af6dd66699
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-02-23 23:10:54 +0530

    created home page

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,13 @@ const baseServerURL = `https://63f45eca3f99f5855dae29dc.mockapi.io`;
 
 // /products
 // /users
+function dropFilters(elemnt){
+    let abc = document.querySelector(`#${elemnt.id} .filter-table-container`);
+    abc.style.visibility = "visible"
+    abc.style.opacity = "1"
+}
+function closeDrpedFilter(elemnt){
+    let abc = document.querySelector(`#${elemnt.id} .filter-table-container`);
+    abc.style.visibility = "hidden"
+    abc.style.opacity = "0"
+}
